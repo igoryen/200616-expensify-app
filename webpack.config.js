f48c846b3a0d168d9c36d7483c489212942e3e7a
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-06-16 17:11:22 -0400

    76. Setting Up Budget App

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js', // The entry point of the app = where the app kicks off
+    output: {
+        path: path.join(__dirname, 'public'), // absolute path + /public
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, 
+            { // added manually
+                test: /\.s?css$/, 
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    devtool: 'eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
+};
