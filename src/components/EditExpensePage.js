9ab9879c22aa831bbdd71383098396beabfd9895
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-06-17 18:02:26 -0400

    81. Query Strings and URL Parameters

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const AddExpensePage = () => (
-    <div>
-        This is from my add expense component
-    </div>
-);
+const AddExpensePage = (props) => {
+    console.log(props);
+    return (
+        <div>
+            Editing the expense with id of {props.match.params.id}
+        </div>
+    )
+};
 
 export default AddExpensePage;
