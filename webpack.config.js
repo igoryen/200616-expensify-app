2d58e9b0a5f3492edd808812e1de7942f95dc8c4
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-06-17 23:59:09 -0400

    85. Setting up Redux

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/app.js', // The entry point of the app = where the app kicks off
+    // entry: './src/app.js', // The entry point of the app = where the app kicks off
+    entry: './src/playground/redux-101.js', // The entry point of the app = where the app kicks off
     output: {
         path: path.join(__dirname, 'public'), // absolute path + /public
         filename: 'bundle.js'
