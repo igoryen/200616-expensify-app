10e23f767f51bd612464dbafe8bd4433dea61e2d
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-06-23 12:27:22 -0400

    89. ES6 Array Destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,28 +1,48 @@
-const person = {
-    name: 'Andrew',
-    age: 27,
-    location: {
-        city: 'Philadelphia',
-        temp: 92
-    }
-};
+// Array destructuring
+const address = [
+    '1299 S Juniper Street', 
+    'Philadelphia', 
+    'Pennsylvania', 
+    '19147'
+];
 
-const { name:firstName = 'Anonymous', age } = person; // if name is undefined, use 'Anonymous'
-console.log(`${firstName} is ${age}`);
+const [street, city, state, zip] = address;
+console.log(`You are in ${city} ${state}`);
 
-const { city, temp: temperature } = person.location; // temp AS temperature
+const [, , state2 = 'New York',] = address;
+console.log(`You are in ${state2}`);
 
-if (city && temperature) {
-    console.log(`It's ${temperature} in ${city}`);
-}
+const item = ['Coffee (iced)', '$3.00', '$3.50', '$3.75'];
+const [itemName, , mediumPrice] = item;
+console.log(`A medium ${itemName} costs ${mediumPrice}`);
 
-const book = {
-    title: 'Ego is the enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        name: 'Penguin'
-    }
-};
+//===========================
+// Object destructuring
+// const person = {
+//     name: 'Andrew',
+//     age: 27,
+//     location: {
+//         city: 'Philadelphia',
+//         temp: 92
+//     }
+// };
 
-const {name:publisherName = 'Self-Published'} = book.publisher;
-console.log(publisherName);
+// const { name:firstName = 'Anonymous', age } = person; // if name is undefined, use 'Anonymous'
+// console.log(`${firstName} is ${age}`);
+
+// const { city, temp: temperature } = person.location; // temp AS temperature
+
+// if (city && temperature) {
+//     console.log(`It's ${temperature} in ${city}`);
+// }
+
+// const book = {
+//     title: 'Ego is the enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// };
+
+// const {name:publisherName = 'Self-Published'} = book.publisher;
+// console.log(publisherName);
