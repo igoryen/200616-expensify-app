1e22c8103ad8c7e72b914a29f4d526afb56ea451
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-06-23 16:44:20 -0400

    93-B. ES6 Spread Operator in Reducers. WITH spread operator.

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -35,7 +35,10 @@ const expensesReducerDefaultState = [];
 const expensesReducer = ( state = expensesReducerDefaultState, action ) => {
     switch (action.type) {
         case 'ADD_EXPENSE':
-            return state.concat(action.expense);
+            return [
+                ...state,
+                action.expense
+            ];
         default:
             return state;
     }
@@ -69,6 +72,8 @@ store.subscribe(() => {
 });
 
 store.dispatch( addExpense({ description: 'Rent', amount: 100 }) );
+store.dispatch( addExpense({ description: 'Coffee', amount: 300 }) );
+
 
 const demoState = {
     expenses: [{
