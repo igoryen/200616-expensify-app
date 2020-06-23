9725b66d5b6f301865ac0ab74e56363c7299b060
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-06-23 15:55:33 -0400

    92-C. Working with Multiple Reducers. Filters Reducer

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -19,10 +19,26 @@ const expensesReducer = ( state = expensesReducerDefaultState, action ) => {
     }
 };
 
+// Filters Reducer
+const filtersReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+};
+const filtersReducer = ( state = filtersReducerDefaultState, action ) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+
 // Store creation
 const store = createStore(
     combineReducers({
-        expenses: expensesReducer
+        expenses: expensesReducer,
+        filters: filtersReducer
     })
 );
 
