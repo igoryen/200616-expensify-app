932f454abc249b28594601447d6236b771c481c3
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-06-25 12:50:35 -0400

    100-C. The Higher Order Component. Pass props to HOC.

--- src/playground/hoc.js
+++ src/playground/hoc.js
@@ -18,7 +18,7 @@ const Info = (props) => (
 const withAdminWarning = (WrappedComponent) => {
     return (props) => (
         <div>
-            <p>This is private info. Please don't share!</p>
+            { props.isAdmin && <p>This is private info. Please don't share!</p>  }
             <WrappedComponent {...props}/>
         </div>
     );
@@ -26,4 +26,4 @@ const withAdminWarning = (WrappedComponent) => {
 
 const AdminInfo = withAdminWarning(Info);
 
-ReactDOM.render(<AdminInfo info="These are the details" />, document.getElementById('app'));
+ReactDOM.render(<AdminInfo isAdmin={true} info="These are the details" />, document.getElementById('app'));
