4108880499c79e71f22dcf616845ff79c0b0c48d
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-06-25 13:48:47 -0400

    101-C. Connecting Store and Component with React-Redux. Render a connected comp.

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const ExpenseList = (props) => (
+    <div>
+        <h1>Expense List</h1>
+        {props.name}
+    </div>
+);
+
+const ConnectedExpenseList = connect( (state) => {
+    return {
+        name: 'Andrew'
+    };
+} )(ExpenseList);
+
+export default ConnectedExpenseList;
