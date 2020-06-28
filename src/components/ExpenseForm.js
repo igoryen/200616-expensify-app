759c735fca560915755cc5a0b5eb2cd872c71935
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-06-28 17:07:23 -0400

    105-C. Creating Expense Add/Edit Form. Added form elements.

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -4,7 +4,22 @@ export default class ExpenseForm extends React.Component {
     render() {
         return (
             <div>
-                ExpenseForm
+                <form>
+                    <input
+                        type="text"
+                        placeholder="Description"
+                        autoFocus
+                    />
+                    <input
+                        type="number"
+                        placeholder="Amount"
+                    />
+                    <textarea
+                        placeholder="Add a note for our expense (optional)"
+                    >
+                    </textarea>
+                    <button>Add Expense</button>
+                </form>
             </div>
         )
     }
