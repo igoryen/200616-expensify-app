3e24594e4a3395c5270aa1918d4503fa0a0cb0be
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-06-28 17:12:44 -0400

    105-D. Creating Expense Add/Edit Form. Connected description to state.

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 export default class ExpenseForm extends React.Component {
+    state = {
+        description: ''
+    };
+    onDescriptionChange = (e) => {
+        const description = e.target.value;
+        this.setState(() => ({description }));
+    };
     render() {
         return (
             <div>
@@ -9,6 +16,8 @@ export default class ExpenseForm extends React.Component {
                         type="text"
                         placeholder="Description"
                         autoFocus
+                        value={this.state.description}
+                        onChange={this.onDescriptionChange}
                     />
                     <input
                         type="number"
