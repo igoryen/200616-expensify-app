6c485aa4d5eaf4c7e2d7f10aa5dea5df4f62ea18
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-06-29 09:58:03 -0400

    107-C. Wiring up Add Expense. Can display new record on dashboard.

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
+import { addExpense } from '../actions/expenses';
 
-const AddExpensePage = () => (
+const AddExpensePage = (props) => (
     <div>
         <h1>Add Expense</h1>
         <ExpenseForm
             onSubmit={(expense) => {
-                console.log(expense);
+                props.dispatch( addExpense(expense) );
             }}
         />
     </div>
 );
 
-export default AddExpensePage;
+export default connect()(AddExpensePage);
