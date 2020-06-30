a536057fad0e3ef35cf18e155a5d3f8a47eec4ea
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-06-30 14:39:37 -0400

    112-D. Setting up Jest. A sample assertion complete.

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -2,4 +2,8 @@ const add = (a, b) => a + b;
 
 test('should add two numbers', () => {
     const result = add(3,4);
+
+    if (result !== 7) {
+        throw new Error(`You added 4 and 3. The result was ${result}. Expected 7`)
+    }
 } );
