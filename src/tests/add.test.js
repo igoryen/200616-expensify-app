8c0127dc938e3976d027837eeff8a5b2e64e7162
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-06-30 14:57:35 -0400

    112-F. Setting up Jest. Test for a string.

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,6 +1,12 @@
 const add = (a, b) => a + b;
+const generateGreeting = (name) => `Hello ${name}!`;
 
 test('should add two numbers', () => {
     const result = add(3,4);
     expect(result).toBe(7);
 } );
+
+test('should generate greeting from name', () => {
+    const result = generateGreeting('Mike');
+    expect(result).toBe('Hello Mike!');
+});
