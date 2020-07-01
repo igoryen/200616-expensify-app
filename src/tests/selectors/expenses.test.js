d0b4fb06fb57ca7cbc5b05fd0664d5dc6071c037
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-07-01 11:39:17 -0400

    115-B. Testing Expenses Selector. By startDate.

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import selectExpenses from '../../selectors/expenses';
 
 const expenses = [{
@@ -11,13 +12,13 @@ const expenses = [{
     description: 'Rent',
     note: '',
     amount: 109500,
-    createdAt: -1000
+    createdAt: moment(0).subtract(4, 'days').valueOf()
 },{
     id: '3',
     description: 'Credit Card',
     note: '',
     amount: 4500,
-    createdAt: 1000
+    createdAt: moment(0).add(4, 'days').valueOf()
 }]
 
 test('should filter by text value', () => {
@@ -31,5 +32,19 @@ test('should filter by text value', () => {
     expect(result).toEqual([
         expenses[2],
         expenses[1]
+    ]);
+});
+
+test('should filter by startDate', () => {
+    const filters = {
+        text: '',
+        sortBy: 'date',
+        startDate: moment(0),
+        endDate: undefined
+    };
+    const result = selectExpenses(expenses, filters);
+    expect(result).toEqual([
+        expenses[2],
+        expenses[0]
     ]);
 });
