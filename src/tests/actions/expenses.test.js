c4ebe143262a04a61d0d9b91f4219bea9510e0ef
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-07-01 01:01:00 -0400

    113-B. Testing Expenses Action Generators. Actions/expenses.

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -7,3 +7,14 @@ test('should setup remove expense action object', () => {
         id: '123abc'
     });
 });
+
+test('should setup edit expense action object', () => {
+    const action = editExpense('123abc', { note: 'New note value' });
+    expect(action).toEqual({
+        type: 'EDIT_EXPENSE',
+        id: '123abc',
+        updates: {
+            note: 'New note value'
+        }
+    });
+});
