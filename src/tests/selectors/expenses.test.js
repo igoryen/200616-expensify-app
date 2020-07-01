f7936fb3bf817c48b3087a06c64d196634a4878e
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-07-01 11:56:45 -0400

    115-C. Testing Expenses Selector. By endDate, by date, by amount.

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -48,3 +48,47 @@ test('should filter by startDate', () => {
         expenses[0]
     ]);
 });
+
+test('should filter by endDate', () => {
+    const filters = {
+        text: '',
+        sortBy: 'date',
+        startDate: undefined,
+        endDate: moment(0).add(2, 'days')
+    };
+    const result = selectExpenses(expenses, filters);
+    expect(result).toEqual([
+        expenses[0],
+        expenses[1]
+    ]);
+});
+
+test('should filter by date', () => {
+    const filters = {
+        text: '',
+        sortBy: 'date',
+        startDate: undefined,
+        endDate: undefined
+    };
+    const result = selectExpenses(expenses, filters);
+    expect(result).toEqual([
+        expenses[2],
+        expenses[0],
+        expenses[1]
+    ]);
+});
+
+test('should filter by amount', () => {
+    const filters = {
+        text: '',
+        sortBy: 'amount',
+        startDate: undefined,
+        endDate: undefined
+    };
+    const result = selectExpenses(expenses, filters);
+    expect(result).toEqual([
+        expenses[1],
+        expenses[2],
+        expenses[0]
+    ]);
+});
