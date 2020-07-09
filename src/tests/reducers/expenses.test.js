8b7591d84f154ff61290455a11bfe2a429bda7c7
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-07-09 16:17:59 -0400

    117-C. Testing Expenses Reducer. removal & id.

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -1,6 +1,28 @@
 import expensesReducer from '../../reducers/expenses';
+import expenses from '../fixtures/expenses';
 
 test('should set default state', () => {
     const state = expensesReducer(undefined, { type: '@@INIT' });
     expect(state).toEqual([]);
+});
+
+test('should remove expense by id', () => {
+    const action = {
+        type: 'REMOVE_EXPENSE',
+        id: expenses[1].id
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([
+        expenses[0],
+        expenses[2]
+    ]);
+});
+
+test('should not remove expenses if id not found', () => {
+    const action = {
+        type: 'REMOVE_EXPENSE',
+        id: '-1'
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual(expenses);
 });
