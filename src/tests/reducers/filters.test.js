7fdebdd3940d3ea0ecb7a4fdc9245fcade2fb823
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-07-09 15:28:45 -0400

    116-C. Testing Filters Reducer. Reducers sort by start & end date

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -27,3 +27,33 @@ test('should set sortBy to date', () => {
     const state = filtersReducer( currentState, action);
     expect(state.sortBy).toBe('date');
 });
+
+test('should set text filter', () => {
+    const text = 'This is my filter';
+    const action  = {
+        type: 'SET_TEXT_FILTER',
+        text
+    };
+    const state = filtersReducer(undefined, action);
+    expect(state.text).toBe(text);
+});
+
+test('should set startDate filter', () => {
+    const startDate = moment();
+    const action = {
+        type: 'SET_START_DATE',
+        startDate
+    };
+    const state = filtersReducer(undefined, action);
+    expect(state.startDate).toEqual(startDate);
+});
+
+test('should set endDate filter', () => {
+    const endDate = moment();
+    const action = {
+        type: 'SET_END_DATE',
+        endDate
+    };
+    const state = filtersReducer(undefined, action);
+    expect(state.endDate).toEqual(endDate);
+});
