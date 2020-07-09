1d9c5000dfc3cccabae4feea50b1ceed13ef0095
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-07-09 16:50:35 -0400

    117-F. Testing Expenses Reducer. should not edit an expense.

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -57,3 +57,16 @@ test('should edit an expense', () => {
     const state = expensesReducer(expenses, action);
     expect(state[1].amount).toBe(amount);
 });
+
+test('should not edit an expense if id not found', () => {
+    const amount = 122000;
+    const action = {
+        type: 'EDIT_EXPENSE',
+        id: -1,
+        updates: {
+            amount
+        }
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual(expenses);
+});
