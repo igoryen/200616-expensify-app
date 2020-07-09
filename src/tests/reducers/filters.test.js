2a96a10e0cfe5bb7b6a5a21fe436c6f45a7d9f97
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-07-09 15:15:56 -0400

    116-B. Testing Filters Reducer. Reducers sort by amt & date

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -10,3 +10,20 @@ test('should set up default filter values', () => {
         endDate: moment().endOf('month')
     });
 });
+
+test('should set sortBy to amount', () => {
+    const state = filtersReducer( undefined, { type: 'SORT_BY_AMOUNT' });
+    expect(state.sortBy).toBe('amount');
+});
+
+test('should set sortBy to date', () => {
+    const currentState = {
+        text: '',
+        startDate: undefined,
+        endDate: undefined,
+        sortBy: 'amount',
+    };
+    const action = { type: 'SORT_BY_DATE'};
+    const state = filtersReducer( currentState, action);
+    expect(state.sortBy).toBe('date');
+});
