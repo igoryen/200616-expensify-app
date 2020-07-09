2a0afde1eb084b2b780fa6d866bcf093071ebbed
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-07-09 16:00:26 -0400

    117. Testing Expenses Reducer. set default state

---
+++ src/tests/reducers/expenses.test.js
@@ -0,0 +1,6 @@
+import expensesReducer from '../../reducers/expenses';
+
+test('should set default state', () => {
+    const state = expensesReducer(undefined, { type: '@@INIT' });
+    expect(state).toEqual([]);
+});
