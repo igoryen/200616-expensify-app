689b5745bef949b95953cf4b11429133f38132b5
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-07-13 01:58:17 -0400

    119-C. Enzyme. Test string.

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -4,7 +4,7 @@ import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
     const wrapper = shallow(<Header />);
-    expect(wrapper.find('h1').length).toBe(1);
+    expect(wrapper.find('h1').text()).toBe("Expensify");
     // const renderer = new ReactShallowRenderer();
     // renderer.render(<Header />);
     // expect(renderer.getRenderOutput()).toMatchSnapshot();
