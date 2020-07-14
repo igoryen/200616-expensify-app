6944950fbde6fe215bb7a8c1e5e4245b76b3e395
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-07-14 12:03:45 -0400

    122-A. Testing User Interaction. Form error.

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -12,3 +12,13 @@ test('should render ExpenseForm correctly with expense data', () => {
     const wrapper = shallow(<ExpenseForm expense={expenses[1]} />);
     expect(wrapper).toMatchSnapshot();
 });
+
+test('should render error for invalid form submission', () => {
+    const wrapper = shallow(<ExpenseForm />);
+    expect(wrapper).toMatchSnapshot();
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => { }
+    });
+    expect(wrapper.state('error').length).toBeGreaterThan(0);
+    expect(wrapper).toMatchSnapshot();
+});
