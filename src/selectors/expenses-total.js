0eab4305a85a1f497bb50b1dcb00f0873609ddbb
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-07-21 16:51:17 -0400

    139C. Build It: Adding Total Selector. Refactored using TDD.

--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -1,11 +1,7 @@
 import expenses from "../tests/fixtures/expenses";
 
 export default (expenses) => {
-    if (expenses.length === 0) {
-        return 0;
-    } else {
-        return expenses
-            .map((expense) => expense.amount)
-            .reduce((sum, value) => sum + value, 0);
-    }
+    return expenses
+        .map((expense) => expense.amount)
+        .reduce((sum, value) => sum + value, 0);
 };
