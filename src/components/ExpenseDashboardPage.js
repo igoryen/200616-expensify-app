9b5b393e0fb0188ca13715ed9b03668008eefc2b
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-07-21 17:39:09 -0400

    140. Build It: Adding Summary Component.

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFiletrs from './ExpenseListFilters';
+import ExpensesSummary from './ExpensesSummary';
 
 const ExpenseDashboardPage = () => (
     <div>
+        <ExpensesSummary />
         <ExpenseListFiletrs />
         <ExpenseList />
     </div>
