5c845c7974adde427f2c2b76a2c0d1111acc90e8
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-07-26 18:30:13 -0400

    152. Asynchronous Redux Actions. Can add items.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,118 +15,120 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-database.ref('expenses').on('child_removed', (snapshot) => {
-    console.log(snapshot.key, snapshot.val());
-});
+export { firebase, database as default };
 
-database.ref('expenses').on('child_changed', (snapshot) => {
-    console.log(snapshot.key, snapshot.val());
-});
-
-database.ref('expenses').on('child_added', (snapshot) => {
-    console.log(snapshot.key, snapshot.val());
-});
-
-// database.ref('expenses')
-//     .once('value')
-//     .then((snapshot) => {
-//         const expenses = [];
-
-//         snapshot.forEach((childSnapshot) => {
-//             expenses.push({
-//                 id: childSnapshot.key,
-//                 ...childSnapshot.val()
-//             });
-//         });
-
-//         console.log(expenses);
-//     });
-
-// database.ref('expenses').on('value', (snapshot) => {
-//     const expenses = [];
-
-//     snapshot.forEach((childSnapshot) => {
-//         expenses.push({
-//             id: childSnapshot.key,
-//             ...childSnapshot.val()
-//         });
-//     });
-
-//     console.log(expenses);
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
 // });
 
-database.ref('expenses').push({
-    description: 'Rent',
-    note: '',
-    amount: 109500,
-    createdAt: 976123498763
-});
-
-
-// database.ref('notes/-MDBOCHLc5L0GUbdVxkL').remove();
-
-// database.ref().on('value', (snapshot) => {
-//     const val = snapshot.val();
-//     console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
 // });
 
-// const onValueChange = database.ref().on('value', (snapshot) => {
-//     console.log(snapshot.val());
-// }, (e) => {
-//     console.log('Error with data fetching', e)
+// database.ref('expenses').on('child_added', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
 // });
 
-// setTimeout(() => {
-//     database.ref('age').set(48)
-// }, 3500);
+// // database.ref('expenses')
+// //     .once('value')
+// //     .then((snapshot) => {
+// //         const expenses = [];
 
-// setTimeout(() => {
-//     database.ref().off(onValueChange);
-// }, 7000);
+// //         snapshot.forEach((childSnapshot) => {
+// //             expenses.push({
+// //                 id: childSnapshot.key,
+// //                 ...childSnapshot.val()
+// //             });
+// //         });
 
-// setTimeout(() => {
-//     database.ref('age').set(50)
-// }, 10500);
+// //         console.log(expenses);
+// //     });
 
-// database.ref('location/city')
-//     .once('value')
-//     .then((snapshot) => {
-//         const val = snapshot.val();
-//         console.log('val: ', val);
-//     })
-//     .catch((e) => {
-//         console.log('Error fetching data', e)
-//     });
+// // database.ref('expenses').on('value', (snapshot) => {
+// //     const expenses = [];
 
-// database.ref().set({
-//     name: 'Igor Entaltsev',
-//     age: 46,
-//     stressLevel: 6,
-//     job: {
-//         title: 'Software developer',
-//         company: 'Google'
-//     },
-//     location: {
-//         city: 'North York',
-//         country: 'Canada'
-//     }
-// }).then(() => {
-//     console.log('Data is saved.')
-// }).catch((error) => {
-//     console.log('This failed.', error)
+// //     snapshot.forEach((childSnapshot) => {
+// //         expenses.push({
+// //             id: childSnapshot.key,
+// //             ...childSnapshot.val()
+// //         });
+// //     });
+
+// //     console.log(expenses);
+// // });
+
+// database.ref('expenses').push({
+//     description: 'Rent',
+//     note: '',
+//     amount: 109500,
+//     createdAt: 976123498763
 // });
 
-// database.ref().update({
-//     stressLevel: 9,
-//     'job/company': 'Amazon',
-//     'location/city': 'Seattle'
-// });
 
-// const everything = database.ref();
-// everything.remove()
-//     .then(() =>  {
-//         console.log("Data was removed.")
-//     })
-//     .catch((error) => {
-//         console.log("Did not remove data: " + error.message)
-//     });
+// // database.ref('notes/-MDBOCHLc5L0GUbdVxkL').remove();
+
+// // database.ref().on('value', (snapshot) => {
+// //     const val = snapshot.val();
+// //     console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+// // });
+
+// // const onValueChange = database.ref().on('value', (snapshot) => {
+// //     console.log(snapshot.val());
+// // }, (e) => {
+// //     console.log('Error with data fetching', e)
+// // });
+
+// // setTimeout(() => {
+// //     database.ref('age').set(48)
+// // }, 3500);
+
+// // setTimeout(() => {
+// //     database.ref().off(onValueChange);
+// // }, 7000);
+
+// // setTimeout(() => {
+// //     database.ref('age').set(50)
+// // }, 10500);
+
+// // database.ref('location/city')
+// //     .once('value')
+// //     .then((snapshot) => {
+// //         const val = snapshot.val();
+// //         console.log('val: ', val);
+// //     })
+// //     .catch((e) => {
+// //         console.log('Error fetching data', e)
+// //     });
+
+// // database.ref().set({
+// //     name: 'Igor Entaltsev',
+// //     age: 46,
+// //     stressLevel: 6,
+// //     job: {
+// //         title: 'Software developer',
+// //         company: 'Google'
+// //     },
+// //     location: {
+// //         city: 'North York',
+// //         country: 'Canada'
+// //     }
+// // }).then(() => {
+// //     console.log('Data is saved.')
+// // }).catch((error) => {
+// //     console.log('This failed.', error)
+// // });
+
+// // database.ref().update({
+// //     stressLevel: 9,
+// //     'job/company': 'Amazon',
+// //     'location/city': 'Seattle'
+// // });
+
+// // const everything = database.ref();
+// // everything.remove()
+// //     .then(() =>  {
+// //         console.log("Data was removed.")
+// //     })
+// //     .catch((error) => {
+// //         console.log("Did not remove data: " + error.message)
+// //     });
