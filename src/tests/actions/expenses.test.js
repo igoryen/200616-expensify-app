9a1b6b8014b5cb1dc3dbfefc1fbd4173aec42a55
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-07-28 12:13:54 -0400

    154A. Testing Async Redux Actions: Part II.

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -2,6 +2,7 @@ import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
+import database from '../../firebase/firebase';
 
 const createMockStore = configureMockStore([thunk]);
 
@@ -43,8 +44,19 @@ test('should add expense to database and store', (done) => {
     };
 
     store.dispatch(startAddExpense(expenseData)).then(() => {
-        expect(1).toBe(1);
-        done();
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'ADD_EXPENSE',
+            expense: {
+                id: expect.any(String),
+                ...expenseData
+            }
+        });
+
+        database.ref(`expenses/${actions[0].expense.id}`).once('value').then((snapshot) => {
+            expect(snapshot.val()).toEqual(expenseData);
+            done();
+        });
     });
 });
 
