05b07d876fe1049f5e6e05b68d0abc33f4a12cca
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-07-28 12:29:24 -0400

    154C. Testing Async Redux Actions: Part II.

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -60,6 +60,32 @@ test('should add expense to database and store', (done) => {
     });
 });
 
+test('should add expense with defaults to database and store', (done) => {
+    const store = createMockStore({});
+    const expenseDefaults = {
+        description: '',
+        amount: 0,
+        note: '',
+        createdAt: 0
+    };
+
+    store.dispatch(startAddExpense({})).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'ADD_EXPENSE',
+            expense: {
+                id: expect.any(String),
+                ...expenseDefaults
+            }
+        });
+
+        return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+    }).then((snapshot) => {
+        expect(snapshot.val()).toEqual(expenseDefaults);
+        done();
+    });
+});
+
 // test('should setup add expense action object with default values', () => {
 //     const action = addExpense();
 //     expect(action).toEqual({
