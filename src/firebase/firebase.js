03e47c91ca4f76ed99ebeea5614006ab863bbecd
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-07-28 13:57:29 -0400

    155. Creating a Separate Test Database.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDHQR8Grd5zQwczQX2YLMTWzDTrvZrQFNo",
-    authDomain: "expensify-56ad3.firebaseapp.com",
-    databaseURL: "https://expensify-56ad3.firebaseio.com",
-    projectId: "expensify-56ad3",
-    storageBucket: "expensify-56ad3.appspot.com",
-    messagingSenderId: "1022530130730",
-    appId: "1:1022530130730:web:7c97ec7e50d009fdf44453",
-    measurementId: "G-1KHZ815M2D"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
   };
 
 firebase.initializeApp(firebaseConfig);
